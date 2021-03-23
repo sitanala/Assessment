85e41f7e520bc41e1773b3e377ab29609a5b690b
sitanala <50581296+sitanala@users.noreply.github.com>
2021-03-23 16:04:27 +0100

    Add files via upload
    Added assessment.

---
+++ protractor.conf.js
@@ -0,0 +1,30 @@
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+exports.config = {
+  debug: false,
+  allScriptsTimeout: 11000,
+  specs: [
+    './e2e/features/*.feature'
+  ],
+  capabilities: {
+    'browserName': 'chrome'
+  },
+  directConnect: true,
+  allScriptsTimeout: 45000,
+  baseUrl: 'http://localhost:4200/',
+  framework: 'custom',
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+     cucumberOpts: {
+      require : [ './e2e/steps/*.steps.ts' ],
+      strict  : false,
+      format  : ['json:test-reports/cucumber-test-results.json'],
+      tags    : ['@smoke','@succes','@fail','@all']
+  },
+  onPrepare() {
+    require('ts-node').register({
+      project: require('path').join(__dirname, './e2e/tsconfig.e2e.json')
+    });
+  }
+  
+};
